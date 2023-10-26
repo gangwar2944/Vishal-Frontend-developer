65fda9bd9f54a13bb0bc57219fd729c877ff6862
gangwar2944 <vishalgangwar094@gmail.com>
2023-10-26 21:03:38 +0530

    added index asset

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import Banner from './components/Banner';
-import Category from './components/Category';
-import Navbar from './components/Navbar';
+import { Suspense, lazy } from "react";
+import "./App.css";
+import LoaderIcon from "./components/LoaderIcon";
 
 function App() {
+  const Home = lazy(() => import("./pages/Home.jsx"));
   return (
     <div className="App">
-         <Navbar/>
-         <Banner/>
-         <Category/>
+      <Suspense fallback={<div className="centered-fallback"><LoaderIcon/></div>}>
+        <Home/>
+      </Suspense>
     </div>
   );
 }
